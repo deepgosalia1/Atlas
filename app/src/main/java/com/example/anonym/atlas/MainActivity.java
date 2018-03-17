d7baf1c80a32513d0c0178b4bf2dceaba703d722
deepgosalia1 <deepgosalia1@gmail.com>
2018-03-17 12:49:20 +0530

    added app features

--- app/src/main/java/com/example/anonym/atlas/MainActivity.java
+++ app/src/main/java/com/example/anonym/atlas/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.anonym.atlas;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by Anonym on 17-03-2018.
@@ -15,6 +23,20 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Intent intent=getIntent();
+        AssetManager assetManager = getAssets();
+        try {
+            InputStream inputStream = assetManager.open("atlas.txt");
+            BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+            String line = null;
+            while((line = in.readLine()) != null) {
+                String IncomingPlace = line.trim();
+                Log.i("IncomingPlace : " , IncomingPlace);
+            }
+        } catch (IOException e) {
+            Toast toast = Toast.makeText(this, "Could not load the Places Directory.", Toast.LENGTH_LONG);
+            toast.show();
+        }
+        
     }
 }
 
