d6611bf793bf95cffaa549c4243b3166d2d2d822
deepgosalia1 <deepgosalia1@gmail.com>
2018-03-17 11:57:27 +0530

    spl_scr.

--- app/src/main/java/com/example/anonym/atlas/MainActivity.java
+++ app/src/main/java/com/example/anonym/atlas/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.anonym.atlas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -12,7 +13,8 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_main);
+        Intent intent=getIntent();
     }
 }
 
--- app/src/main/java/com/example/anonym/atlas/SplashActivity.java
+++ app/src/main/java/com/example/anonym/atlas/SplashActivity.java
@@ -1,12 +1,46 @@
 package com.example.anonym.atlas;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.felipecsl.gifimageview.library.GifImageView;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class SplashActivity extends AppCompatActivity {
 
     private int splash_timeout = 4000;
+    private GifImageView gifImageView;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
+        gifImageView=(GifImageView)findViewById(R.id.gifImageView);
 
+        try{
+            InputStream inputStream= getAssets().open("splashscreen.gif");
+            byte[] bytes= IOUtils.toByteArray(inputStream);
+            gifImageView.setBytes(bytes);
+            gifImageView.startAnimation();
+        }
+        catch (IOException e){
+            Toast.makeText(this, "Sorry some error occured while loading the screen.", Toast.LENGTH_SHORT).show();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SplashActivity.this.startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                SplashActivity.this.finish();
+            }
+        },4000);
+    }
 }
