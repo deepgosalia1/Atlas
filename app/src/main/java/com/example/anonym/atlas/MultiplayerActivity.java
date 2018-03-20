16fa9080b95f8ec9aca3d61bca89dc472b11142d
pranavgor <pranavgor07@gmail.com>
2018-03-21 01:44:45 +0530

    set hint vala sala nai samaj raha

--- app/src/main/java/com/example/anonym/atlas/MultiplayerActivity.java
+++ app/src/main/java/com/example/anonym/atlas/MultiplayerActivity.java
@@ -5,6 +5,8 @@ import android.content.res.AssetManager;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -29,7 +31,8 @@ public class MultiplayerActivity extends AppCompatActivity {
     LinkedHashMap<String,String> all_place = new LinkedHashMap<>(46354);
     LinkedHashMap<String,String> all_copy = new LinkedHashMap<>(46354);
     TextView player1_place,player2_place,turnindicator;
-    EditText player1_edittext,player2_edittext;
+    EditText player1_edit;
+    EditText player2_edit;
     boolean player1turn=false;
     int firstuse=0;
     Character x = '#';
@@ -37,93 +40,58 @@ public class MultiplayerActivity extends AppCompatActivity {
 
     //button methods
     public void submit1(View view) {
-        player1_edittext.setHint("");
-        if (player1_edittext.getText().toString() == "") {
+        player1_edit.setHint("\t");
+        if (TextUtils.isEmpty(player1_edit.getText().toString())) {
             Toast.makeText(this, "Please enter a name of the place first.", Toast.LENGTH_LONG).show();
-        } else {
-            placeEntered = player1_edittext.getText().toString().toLowerCase();
+        }
+        else {
+            placeEntered = player1_edit.getText().toString().toLowerCase();
             Character temp = placeEntered.charAt(0);
-            if (firstuse == 1) {
-                player1_place.setText(placeEntered);
-                if (all_copy.containsKey(placeEntered)) {
-                    Toast.makeText(this, "Player 1 place verified ! , turn change.", Toast.LENGTH_SHORT).show();
-                    place_to_be_passed1 = placeEntered;
-                    keys.remove(placeEntered.toLowerCase());
-                    all_copy.remove(placeEntered.toLowerCase());
-                    x = placeEntered.charAt(placeEntered.length() - 1);
-                    player2_edittext.setHint("Enter place from " + x.toString() + "... ");
-                    turnindicator.setText("Player 2's turn");
-                    player1_edittext.setText("");
-                    firstuse = 0;
-
-                } else if (!keys.contains(placeEntered.toLowerCase())) {
-                    Toast.makeText(this, "Place not found. pLayer 1 looses. New Game!", Toast.LENGTH_SHORT).show();
-                    onStart(null);
-                }
-
-            } else {
-
-                if ((all_copy.containsKey(placeEntered) && firstuse == 1) || (all_copy.containsKey(placeEntered) && temp == x)) {
-                    String place = all_copy.get(placeEntered);
-                    player1_place.setText(place);
-                    place_to_be_passed1 = placeEntered;
-                    all_copy.remove(placeEntered);
-                    keys.remove(placeEntered);
-                    x = placeEntered.charAt(placeEntered.length() - 1);
-                    firstuse = 0;
-                    player1_edittext.setText("");
-                    player2_edittext.setHint("Enter place from " + x.toString() + "... ");
-                    turnindicator.setText("Player 2's turn");
-                } else if (!all_copy.containsKey(placeEntered) || temp != x) {
-                    Toast.makeText(this, "Place invalid or repeated, Hence Player 2 Wins!", Toast.LENGTH_LONG).show();
-                    onStart(null);
-                }
+            Log.d("user-place index 0 : ", temp.toString());
+            Log.d("comp-place index end : ", x.toString());
+            Log.d("firstuse : ", String.valueOf(firstuse));
+            if ((all_copy.containsKey(placeEntered) && firstuse == 1) || (all_copy.containsKey(placeEntered) && temp == x)) {
+                String place = all_copy.get(placeEntered);
+                player1_place.setText(place);
+                place_to_be_passed1 = placeEntered;
+                all_copy.remove(placeEntered);
+                keys.remove(placeEntered);
+                x = placeEntered.charAt(placeEntered.length() - 1);
+                firstuse = 0;
+                player1_edit.setText("");
+                player2_edit.setHint("Enter place from " + x.toString() + "... ");
+                turnindicator.setText("Player 2's turn");
+            }
+            else if (!all_copy.containsKey(placeEntered) || temp != x) {
+                Toast.makeText(this, "Place invalid or repeated, Player 2 Wins!", Toast.LENGTH_LONG).show();
+                onStart(null);
             }
         }
     }
 
     public void submit2(View view){
-        player2_edittext.setHint("");
-        if (player2_edittext.getText().toString() == "") {
+        player2_edit.setHint("\t");
+        if (TextUtils.isEmpty(player2_edit.getText().toString())) {
             Toast.makeText(this, "Please enter a name of the place first.", Toast.LENGTH_LONG).show();
-        } else {
-            placeEntered = player2_edittext.getText().toString().toLowerCase();
+        }
+        else {
+            placeEntered = player2_edit.getText().toString().toLowerCase();
             Character temp = placeEntered.charAt(0);
-            if (firstuse == 1) {
-                player2_place.setText(placeEntered);
-                if (all_copy.containsKey(placeEntered)) {
-                    Toast.makeText(this, "Player 2 place verified ! , turn change.", Toast.LENGTH_SHORT).show();
-                    place_to_be_passed2 = placeEntered;
-                    keys.remove(placeEntered.toLowerCase());
-                    all_copy.remove(placeEntered.toLowerCase());
-                    x = placeEntered.charAt(placeEntered.length() - 1);
-                    player2_edittext.setText("");
-                    player1_edittext.setHint("Enter place from " + x.toString() + "... ");
-                    turnindicator.setText("Player 1's turn");
-                    firstuse = 0;
-
-                } else if (!keys.contains(placeEntered.toLowerCase())) {
-                    Toast.makeText(this, "Place not found. pLayer 2 looses. New Game!", Toast.LENGTH_SHORT).show();
-                    onStart(null);
-                }
-
-            } else {
-
-                if ((all_copy.containsKey(placeEntered) && firstuse == 1) || (all_copy.containsKey(placeEntered) && temp == x)) {
-                    String place = all_copy.get(placeEntered);
-                    player1_place.setText(place);
-                    place_to_be_passed2 = placeEntered;
-                    all_copy.remove(placeEntered);
-                    keys.remove(placeEntered);
-                    x = placeEntered.charAt(placeEntered.length() - 1);
-                    firstuse = 0;
-                    player1_edittext.setHint("Enter place from " + x.toString() + "... ");
-                    player2_edittext.setText("");
-                    turnindicator.setText("Player 1's turn");
-                } else if (!all_copy.containsKey(placeEntered) || temp != x) {
-                    Toast.makeText(this, "Place invalid or repeated, Hence Player 1 Wins!", Toast.LENGTH_LONG).show();
-                    onStart(null);
-                }
+            if ((all_copy.containsKey(placeEntered) && firstuse == 1) || (all_copy.containsKey(placeEntered) && temp == x)) {
+                String place = all_copy.get(placeEntered);
+                player1_place.setText(place);
+                place_to_be_passed2 = placeEntered;
+                all_copy.remove(placeEntered);
+                keys.remove(placeEntered);
+                x = placeEntered.charAt(placeEntered.length() - 1);
+                firstuse = 0;
+                player1_edit.setHint("Enter place from " + x.toString() + "... ");
+                player2_edit.setText("");
+                turnindicator.setText("Player 1's turn");
+            }
+            else if (!all_copy.containsKey(placeEntered) || temp != x) {
+                Toast.makeText(this, "Place invalid or repeated, Hence Player 1 Wins!", Toast.LENGTH_LONG).show();
+                onStart(null);
             }
         }
     }
@@ -148,15 +116,15 @@ public class MultiplayerActivity extends AppCompatActivity {
 
     public boolean onStart(View view) {
         player1turn = random.nextBoolean();
-        player1_edittext.setHint("");
-        player2_edittext.setHint("");
+        player1_edit.setText("");
+        player2_edit.setText("");
+        player1_edit.setHint("\t");
+        player2_edit.setHint("\t");
         firstuse=1;
         all_copy.putAll(all_place);
         for(Map.Entry<String, String> t : all_place.entrySet()) {
             keys.add(t.getKey());
         }
-        player1_edittext.setText("");
-        player2_edittext.setText("");
         if (player1turn) {
             turnindicator.setText("Player 1's turn!");
             new Handler().postDelayed(new Runnable() {
@@ -166,7 +134,7 @@ public class MultiplayerActivity extends AppCompatActivity {
                     Toast.makeText(MultiplayerActivity.this, "Player 1's turn",Toast.LENGTH_SHORT).show();
                 }
             }, 2000);
-            player1_edittext.setHint("player 1 enter a random place..");
+            player1_edit.setHint("player 1 enter a random place..");
         } else {
             turnindicator.setText("Player 2's turn");
             new Handler().postDelayed(new Runnable() {
@@ -176,7 +144,7 @@ public class MultiplayerActivity extends AppCompatActivity {
                     Toast.makeText(MultiplayerActivity.this, "Player 2's Turn",Toast.LENGTH_SHORT).show();
                 }
             }, 2000);
-            player2_edittext.setHint("player 2 enter a random place..");
+            player2_edit.setHint("player 2 enter a random place..");
         }
         return true;
     }
@@ -191,8 +159,9 @@ public class MultiplayerActivity extends AppCompatActivity {
         player2_place=(TextView)findViewById(R.id.player1_place);
         player2_place=(TextView)findViewById(R.id.player2_place);
         turnindicator=(TextView)findViewById(R.id.turnindicator);
-        player1_edittext=(EditText)findViewById(R.id.player1_edittext);
-        player2_edittext=(EditText) findViewById(R.id.player2_edittext);
+        player1_edit=(EditText)findViewById(R.id.player1_edittext);
+        player2_edit=(EditText)findViewById(R.id.player2_edittext);
+        Log.i("Edit text : ",player2_edit.getHint().toString());
         AssetManager assetManager = getAssets();
         try{
             inputStream = assetManager.open("atlas.txt");
